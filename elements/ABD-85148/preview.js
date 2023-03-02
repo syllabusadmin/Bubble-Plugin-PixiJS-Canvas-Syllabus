d3f12d0ff53bfac9077e26ad1a14dea934630b0d
syllabusadmin <126100055+syllabusadmin@users.noreply.github.com>
2023-03-02 12:12:46 -0800

    Automatic commit from Bubble

--- elements/ABD-85148/preview.js
+++ elements/ABD-85148/preview.js
@@ -1,5 +1,5 @@
 function(instance, properties) {
 
-
+    instance.canvas.insertAdjacentHTML(`beforeend`, `<img src ="//meta.cdn.bubble.io/f1677787035935x986844379014684400/canvas%20example%20screenshot.png"></img>`)
 
 }
