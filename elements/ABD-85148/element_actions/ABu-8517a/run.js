98e10b3e952b34bd56beffe9773dea5268ff5fb0
syllabusadmin <126100055+syllabusadmin@users.noreply.github.com>
2023-06-05 08:52:42 -0700

    Automatic commit from Bubble

--- elements/ABD-85148/element_actions/ABu-8517a/run.js
+++ elements/ABD-85148/element_actions/ABu-8517a/run.js
@@ -1,14 +1,23 @@
 function(instance, properties, context) {
-
-
-  //Load any data 
-const findDAS = instance.data.rectangles.filter(item => item.id === properties.das);
-let newPosition = (findDAS[0].y + properties.ypadding) * -1;
-console.log("findDas",findDAS, newPosition);
-instance.data.mainContainer.position.y = newPosition;
-
-  //Do the operation
-
-
-
+    //find the instances of DAS in this PIXI instance
+    const findDAS = instance.data.rectangles.filter(item => item.id === properties.das);
+    //Set infinity
+    const inf = 1 / 0;
+    let newPosition = 0;
+    //find the first item that has a y value
+    for (let i = 0; i < findDAS.length; i++) {
+        if (findDAS[i].y !== inf && findDAS[i].y !== null && typeof findDAS[i].y !== "undefined") {
+            newPosition = (findDAS[i].y + properties.ypadding) * -1;
+            break;
+        }
+    }
+    //if there is an item, scroll to it
+    if (newPosition !== 0) {
+        properties.logging ? console.log("findDas", findDAS, newPosition) : null;
+        instance.data.mainContainer.position.y = newPosition;
+    } else {
+        properties.logging ? console.log("findDas Y empty") : null;
+    }
+    //debugging
+    properties.logging ? window.PCS = instance : null;
 }
